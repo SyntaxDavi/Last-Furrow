ce180a45d6197fee25cdd9b673bc48bc75ae4bf6
Davi Arthur Muller <Davi_arthur_muller@outlook.com>
2026-01-19 01:32:10 -0300

    Refatoration point

--- Assets/Scripts/UI/Core/UIBootstrapper.cs
+++ Assets/Scripts/UI/Core/UIBootstrapper.cs
@@ -59,7 +59,7 @@ public class UIBootstrapper : MonoBehaviour
         var context = CreateUIContext();
 
         // 2. Injeta em HeartDisplayManagerRefactored
-        var heartManager = FindObjectOfType<HeartDisplayManagerV2>();
+        var heartManager = FindFirstObjectByType<HeartDisplayManagerV2>();
         if (heartManager != null)
         {
             heartManager.Initialize(context);
@@ -68,7 +68,7 @@ public class UIBootstrapper : MonoBehaviour
         }
 
         // 3. Injeta em DayWeekDisplayRefactored
-        var dayWeekDisplay = FindObjectOfType<DayWeekDisplayV2>();
+        var dayWeekDisplay = FindFirstObjectByType<DayWeekDisplayV2>();
         if (dayWeekDisplay != null)
         {
             dayWeekDisplay.Initialize(context);
@@ -77,7 +77,7 @@ public class UIBootstrapper : MonoBehaviour
         }
 
         // 4. Injeta em SleepButtonControllerRefactored
-        var sleepButton = FindObjectOfType<SleepButtonControllerV2>();
+        var sleepButton = FindFirstObjectByType<SleepButtonControllerV2>();
         if (sleepButton != null)
         {
             sleepButton.Initialize(context);
